d0a8c4f62a66b328e50f9108277a385e82765d67
AymanShawqi <aymanshawqi2017@gmail.com>
2018-09-29 21:30:19 +0200

    Initial commit

---
+++ app/src/main/java/com/android/fragmentscommunicatingwitharguments/myFragments/FragmentOne.java
@@ -0,0 +1,62 @@
+package com.android.fragmentscommunicatingwitharguments.myFragments;
+
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.fragmentscommunicatingwitharguments.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FragmentOne extends Fragment {
+
+    private String name;
+    private int age;
+
+    public FragmentOne() {
+        // Required empty public constructor
+    }
+
+    public static FragmentOne newInstanceOf(String name,int age){
+        FragmentOne fragmentOne=new FragmentOne();
+        Bundle args=new Bundle();
+        args.putString("name",name);
+        args.putInt("age",age);
+        fragmentOne.setArguments(args);
+        return fragmentOne;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+      //  name=getArguments().getString("name"," ");
+      //  age=getArguments().getInt("age",0);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_one, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        name=getArguments().getString("name"," ");
+        age=getArguments().getInt("age",0);
+
+        TextView greatTxt=view.findViewById(R.id.greatingMsg);
+        String great="Welcome "+name+" Your Age is "+age;
+        greatTxt.setText(great);
+    }
+}
